04e3b2c5f551a1a55751c2f7e907db0d8bdeadcc
ser u <user@el-capitan-cs6-3.local>
2016-08-25 14:49:03 +0900

    Initial Commit

---
+++ viewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  viewController
+//
+//  Created by user on 2016. 8. 25..
+//  Copyright © 2016년 MarsCorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ viewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  viewController
+//
+//  Created by user on 2016. 8. 25..
+//  Copyright © 2016년 MarsCorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
